c48e7566cd4aedfe51e336a4c641468af0943dcc
Zack Winchell <zackwinchell@gmail.com>
2016-02-05 10:45:53 -0800

    User's albums now populate /albums on load

--- public/js/albums.js
+++ public/js/albums.js
@@ -2,11 +2,12 @@
 
 $(init);
 
-var $title;
+var $title, $albumListings;
 
 function init() {
   $("#createAlbum").submit(doCreateAlbum);
   $title = $("#title");
+  $albumListings = $("#albumListings");
 }
 
 function doCreateAlbum(e) {
@@ -15,8 +16,15 @@ function doCreateAlbum(e) {
   .success(function(data) {
     $title.val("");
     console.log("Saved:", data);
+    albumPostCreate(data.title);
   })
   .fail(function(err) {
     console.warn(err);
   });
+}
+
+function albumPostCreate(title) {
+  var listing = $("<div>").addClass("col-xs-4 listing");
+  listing.append($("<h2>").text(title));
+  $albumListings.append(listing);
 }
