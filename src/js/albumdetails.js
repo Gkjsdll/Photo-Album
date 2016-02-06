259407596f18f0b9518b7d1c3aeae706aa0734cb
Zack Winchell <zackwinchell@gmail.com>
2016-02-06 01:08:48 -0800

    removed buttons with incomplete functionality for project submission

--- src/js/albumdetails.js
+++ src/js/albumdetails.js
@@ -3,5 +3,27 @@
 $(init);
 
 function init() {
+  // $("#images button").click(doImageDelete);
   // this did something once upon a time
+}
+
+function doImageDelete() {
+  // swal({
+  //   title: "Are you sure you would like to delete this image?",
+  //   text: "This action cannot be undone.",
+  //   showCancelButton: true,
+  //   confirmButtonText: "Yes, Delete It!",
+  //   cancelButtoonText: "No, Keep It!",
+  //   closeOnConfirm: false,
+  //   closeOnCancel: false
+  // }, function(isConfirm) {
+  //   if(isConfirm) {
+  //     debugger;
+  //     //delete the image
+  //     swal("Your image was deleted", "", "error");
+  //   }
+  //   else {
+  //     swal("Your image was not deleted", "", "success");
+  //   }
+  // });
 }
